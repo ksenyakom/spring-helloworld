be71cdadaa971f2a25e9244bab1007606f7f0cf2
ksenys kom <ksenyakom@gmail.com>
2020-10-18 20:04:48 +0300

    1 commit cat added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("bean = bean1 "  + (bean == bean1));
+        System.out.println("catBean = catBean1 " + (catBean == catBean1));
     }
 }
